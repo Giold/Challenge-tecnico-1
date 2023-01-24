877b87a3b7cbe2833fd1d35ffacc83f51187a6f3
Giold <64499571+Giold@users.noreply.github.com>
2023-01-24 17:15:01 -0600

    Update2

--- Challege 1.py
+++ Challege 1.py
@@ -1,8 +1,8 @@
 #Datos para la operacion#  
 Latitud1 = 18.60646 
-Latitud2 = -90.73780
-Longitud1 = -38.373825292521154
+Longitud1 = -90.73780
+Latitud2 = -38.373825292521154
 Longitud2 =-69.67405073588125 
 tiempo = 24 
-print("Soy la papa")
+
 
