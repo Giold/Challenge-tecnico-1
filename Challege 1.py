951ad98cdbbf177ee9e098b1eee334d91d9bc9e9
Giold <64499571+Giold@users.noreply.github.com>
2023-01-25 10:19:42 -0600

    Update 3

--- Challege 1.py
+++ Challege 1.py
@@ -3,17 +3,18 @@ import math
 
 #Datos para la operacion#  
 Latitud1 = 18.60646 
-Latitud1cos = math.cos(Latitud1)
+Latitud1rad = (math.pi*Latitud1)/180
+Latitud1cos = math.cos(Latitud1rad)
 Longitud1 = -90.73780
 Latitud2 = -38.373825292521154
-Latitud2cos = math.cos(Latitud2)
+Latitud2rad = (math.pi*Latitud2)/180
+Latitud2cos = math.cos(Latitud2rad)
 Longitud2 = -69.67405073588125 
 tiempo = 24 
 
-v = (((math.pi*2)*(6371))*Latitud1cos)/24
-v1 = (((math.pi*2)*(6371))*Latitud2cos)/24
+v = (((math.pi*2)*(6371))*Latitud1cos)/tiempo
+v1 = (((math.pi*2)*(6371))*Latitud2cos)/tiempo
 
 
 print(v)
-print(v1)
- 
+print(v1) 
