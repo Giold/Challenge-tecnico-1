265e9993f30fe6ed660c8af425a49180ea07d0e6
Giold <64499571+Giold@users.noreply.github.com>
2023-02-05 16:24:31 -0600

    avance 2

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+##Funcion para que detecte HTML
+@app.route('/')
+
+def index():
+    
+    ##Lista de datos para probar impresion
+    clases_escuela = ['Quimica','Espanol','Fisica', 'Ingles']
+    
+    #Datos a imprimir 
+    data = {
+        'titulo' : 'Prueba Server PAPA al cuadrado',
+        'bienvenida' : 'Hello There!',
+        'clases' : clases_escuela,
+        'numero_clases' : len(clases_escuela)
+        
+    }
+    
+    return render_template('index.html', data=data)
+
+##Dato para abrir el servidor con el puerto
+if __name__== '__main__':
+    app.run(debug=True, port=5000)
