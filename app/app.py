acb98632c1b1dcf803cc594783b670553b595c08
Giold <64499571+Giold@users.noreply.github.com>
2023-02-14 01:58:04 -0600

    avance1

--- app/app.py
+++ app/app.py
@@ -1,51 +1,28 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify
+from config import config
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
 
-# Datos necesarios para conectar a base de datos
+app = Flask(__name__)
+conexion = MySQL(app)
+
 
 ##Funcion para que detecte HTML
-@app.route('/')
+@app.route('/pokemons')
+def lista_pokemon():
+    try:
+        return 'ok'
+    except Exception as ex:
+        return 'Error'
+
+
 
 def index():
-    
-    ##Lista de datos para probar impresion
-    clases_escuela = ['Quimica','Espanol','Fisica', 'Ingles']
-    
-    #Datos a imprimir 
-    data = {
-        'titulo' : 'Prueba Server PAPA al cuadrado',
-        'bienvenida' : 'Hello There!',
-        'clases' : clases_escuela,
-        'numero_clases' : len(clases_escuela)
-        
-    }
-    
-    return render_template('index.html', data=data)
+    return "Evola"
+   
 
-@app.route('/contacto/<nombre>/<int:edad>')
-def contacto(nombre, edad):
-    data={
-        'titulo':'Contacto',
-        'nombre':nombre,
-        'edad':edad
-
-    }
-    return render_template('contacto.html', data=data)
-
-def query_string():
-    print(request)
-    print(request.args)
-    print(request.args.get('param1'))
-    print(request.args.get('param2'))
-    return "Ok"
-
-def pagina404(error):
-    return render_template('404.html'), 404
 
 ##Dato para abrir el servidor con el puerto
 if __name__== '__main__':
-    app.add_url_rule('/query_string',view_func=query_string)
-    app.register_error_handler(404,pagina404)
-    app.run(debug=True, port=5000)
+    app.config.from_object(config['development'])
+    app.run()
