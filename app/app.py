770198a3be68bb2adb3d75d258886b1fb7a5ea72
Giold <64499571+Giold@users.noreply.github.com>
2023-02-15 01:55:43 -0600

    eso

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask
 from config import config
 from flask_mysqldb import MySQL
+
+
 
 
 app = Flask(__name__)
@@ -11,18 +13,21 @@ conexion = MySQL(app)
 @app.route('/pokemons')
 def lista_pokemon():
     try:
-        return 'ok'
+        cursor=conexion.connection.cursor()
+        sql="SELECT Nombre_pokemon, Abilidad, Limite, FROM api_pokemon_prueba"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        print(datos)
     except Exception as ex:
         return 'Error'
 
-
-
-def index():
-    return "Evola"
+def page_not_found(error):
+    return "<h1>Oppppppps!!!!!!!!!!!</h1><h2>The page you were searching Doesn´t exist </h2>"
    
 
 
 ##Dato para abrir el servidor con el puerto
 if __name__== '__main__':
     app.config.from_object(config['development'])
+    app.register_error_handler(404, page_not_found)
     app.run()
--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 class DevelopmentConfig():
     DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'Alex_Pruebas'
+    MYSQL_PASSWORD = 'lG.(]@XUOii@ygbA'
+    MYSQL_DB = 'api_pokemon_prueba'
 
 config = {
     'development': DevelopmentConfig
